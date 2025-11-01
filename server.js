ea0cd838f728105e0b6a5e3ff15aa641a2830730
PhatanNagulSharif <159712363+sharif3170@users.noreply.github.com>
2025-11-01 11:04:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 // 📌 CORS Configuration
 // =======================
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "https://food-habits.vercel.app/",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
   credentials: true,
